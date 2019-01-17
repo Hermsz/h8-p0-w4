620f6c72b2953826274737f5b2177db839f2b41c
hermsz <email@hermi.ltd>
2019-01-17 14:53:56 +0700

    Add file

--- Practice1-Object.js
+++ Practice1-Object.js
@@ -1,3 +1,7 @@
+
+
+var data = []
+
 var array = [
   [1, "imam", 21, false, "bakso"],
   [2, "tulus", 22, true, "nasi goreng"],
@@ -6,11 +10,13 @@ var array = [
 
 for (var i = 0; i < array.length; i++) {
   var object = {};
-  object.no = array[i][j];
-  object.name = array[i][j];
-  object.name = array[i][j + 1];
-  object.name = array[i][j + 1];
-  object.name = array[i][j + 1];
+  for (var j = 0; j < array[i].length; j++) {
+  object.no = array[i][0];
+  object.name = array[i][1];
+  object.age = array[i][2];
+  object.available = array[i][3];
+  object.mafa = array[i][4];
+  }
 
   data.push(object);
 }
--- w4-Ex011.js
+++ w4-Ex011.js
@@ -7,16 +7,20 @@ Diberikan sebuah function shoppingTime(memberId, money) yang menerima dua parame
 
 Toko X sedang melakukan SALE untuk beberapa barang, yaitu:
 
-Sepatu brand Stacattu seharga 1500000
-Baju brand Zoro seharga 500000
-Baju brand H&N seharga 250000
-Sweater brand Uniklooh seharga 175000
-Casing Handphone seharga 50000
+- Sepatu brand Stacattu seharga 1500000
+- Baju brand Zoro seharga 500000
+- Baju brand H&N seharga 250000
+- Sweater brand Uniklooh seharga 175000
+- Casing Handphone seharga 50000
+
 Buatlah function yang akan mengembalikan sebuah object dimana object tersebut berisikan informasi memberId, money, listPurchased dan changeMoney.
 
 Jika memberId kosong maka tampilkan "Mohon maaf, toko X hanya berlaku untuk member saja"
+
 Jika uang yang dimiliki kurang dari 50000 maka tampilkan "Mohon maaf, uang tidak cukup"
+
 Member yang berbelanja di toko X akan membeli barang yang paling mahal terlebih dahulu dan akan membeli barang-barang yang sedang SALE masing-masing 1 jika uang yang dimilikinya masih cukup.
+
 Contoh jika inputan memberId: '324193hDew2' dan money: 700000
 
 maka output:
@@ -27,7 +31,45 @@ maka output:
 
 function shoppingTime(memberId, money) {
 
+  var listProduct = {  // Data from the question above now displayed as Object
+    'Sepatu Stacattu': 1500000,
+    'Baju Zoro' : 500000,
+    'Baju H&N': 250000,
+    'Sweater Unikloooh': 175000,
+    'Casing Handphone': 50000
+  }
+  var arrKey = Object.keys(listProduct) // Extract keys from object above and convert it to Array
+
+  var arrValue = Object.values(listProduct) // Extract values from object above and convert it to Array
+
+  if( memberId === undefined || money === '' ) {
+    return 'Mohon maaf, toko X hanya berlaku untuk member saja'
+  } else if( memberId === '') {
+    return 'Mohon maaf, toko X hanya berlaku untuk member saja'
+  } else if( money < 50000) {
+    return 'Mohon maaf, uang tidak cukup'
+  } else {
+
+    var object = {}
+    var array = []
+    object.memberId = memberId
+    object.money = money
+
+    for( var i = 0; i <= arrKey.length - 1; i++ ) {
+      if( money >= arrValue[i] ) {
+        array.push(arrKey[i])
+        money -= arrValue[i]
+      } 
+  
+    object.listPurchased = array,
+    object.changeMoney = money
+    }
+  }
+
+  return object
 }
+
+
 
 // TEST CASES
 console.log(shoppingTime('1820RzKrnWn08', 2475000));
@@ -40,12 +82,13 @@ console.log(shoppingTime('1820RzKrnWn08', 2475000));
   //    'Sweater Uniklooh',
   //    'Casing Handphone' ],
   // changeMoney: 0 }
+
 console.log(shoppingTime('82Ku8Ma742', 170000));
-//{ memberId: '82Ku8Ma742',
-// money: 170000,
-// listPurchased:
-//  [ 'Casing Handphone' ],
-// changeMoney: 120000 }
+// //{ memberId: '82Ku8Ma742',
+// // money: 170000,
+// // listPurchased:
+// //  [ 'Casing Handphone' ],
+// // changeMoney: 120000 }
 console.log(shoppingTime('', 2475000)); //Mohon maaf, toko X hanya berlaku untuk member saja
 console.log(shoppingTime('234JdhweRxa53', 15000)); //Mohon maaf, uang tidak cukup
 console.log(shoppingTime()); ////Mohon maaf, toko X hanya berlaku untuk member saja
